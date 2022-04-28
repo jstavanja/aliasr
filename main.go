70e3e85d7d12c577e779ccce98f16658fabcc1f9
jstavanja <jakastavanja@gmail.com>
2022-04-28 21:01:16 +0200

    add a TUI for selecting workspaces found in the kv db

--- main.go
+++ main.go
@@ -1,11 +1,65 @@
 package main
 
 import (
+  "fmt"
+  "os"
   "log"
   "strings"
 
   "github.com/charmbracelet/charm/kv"
+  tea "github.com/charmbracelet/bubbletea"
 )
+
+type model struct {
+  workspaces []string
+  cursor int
+}
+
+func (m model) Init() tea.Cmd {
+  return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+  switch msg := msg.(type) {
+    case tea.KeyMsg:
+      switch msg.String() {
+        case "ctrl+c", "q":
+          return m, tea.Quit
+
+        case "up", "k":
+          if m.cursor > 0 {
+            m.cursor--
+          }
+
+        case "down", "j":
+          if m.cursor < len(m.workspaces)-1 {
+            m.cursor++
+          }
+
+          case "enter", " ":
+            // TODO: make this enter a workspace
+      }
+  }
+
+  return m, nil
+}
+
+func (m model) View() string {
+  s := "Choose your workspace:\n"
+  for i, choice := range m.workspaces {
+
+    cursor := " "
+    if m.cursor == i {
+        cursor = ">"
+    }
+
+    s += fmt.Sprintf("%s %s\n", cursor, choice)
+  }
+
+  s += "\nPress q to quit.\n"
+
+  return s
+}
 
 func main() {
   // Open or create the aliasr database that will persist the user's data locally 
@@ -39,7 +93,14 @@ func main() {
   // Create a slice with workspaces from the db string value
   workspaces := strings.Split(string(v), ",")
 
-  for _, workspace := range workspaces {
-   log.Println(workspace)
+  initialModel := model{
+    workspaces: workspaces,
+  }
+
+  p := tea.NewProgram(initialModel)
+
+  if err := p.Start(); err != nil {
+    fmt.Printf("Alas, there's been an error: %v", err)
+    os.Exit(1)
   }
 }
