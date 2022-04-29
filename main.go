ae4668a6439c24fa6540009abf2c762ef782b4b7
jaka.stavanja <jaka.stavanja@oryxgaming.com>
2022-04-29 16:57:11 +0200

    add support for reading workspace commands from the kv db

--- main.go
+++ main.go
@@ -12,9 +12,11 @@ import (
 
 type model struct {
   screen string
-  currentWorkspace int
+  currentWorkspace string
   workspaces []string
   cursor int
+  commandsInCurrentWorkspace []string
+  db *kv.KV
 }
 
 func (m model) Init() tea.Cmd {
@@ -45,11 +47,13 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
         case "enter", " ":
           if m.screen == "workspace-list" {
-            m.currentWorkspace = m.cursor
+            m.currentWorkspace = m.workspaces[m.cursor]
+            m.commandsInCurrentWorkspace = getCommmandsForCurrentWorkspace(m)
             m.cursor = 0
             m.screen = "workspace-detail"
           } else {
-            // get the command under the cursor and execute it
+            // TODO: get the command under the cursor and execute it, like:
+            // exec.Command(m.commandsInCurrentWorkspace[m.cursor])
           }
       }
   }
@@ -57,8 +61,32 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
   return m, nil
 }
 
+func getCommmandsForCurrentWorkspace(m model) []string {
+  // Get the persisted workspace commands
+  v, err := m.db.Get([]byte("workspace-commands")) // format: workspace_name:command,command|workspace_name_2:command,command
+
+  if err != nil || len(v) == 0 {
+    return []string{}
+  }
+
+  workspaces_with_commands := strings.Split(string(v), "|")
+
+  for _, workspace_with_commands := range workspaces_with_commands {
+    workspace_name_and_commands_list := strings.Split(workspace_with_commands, ":")
+
+    workspace_name := workspace_name_and_commands_list[0]
+    commands := workspace_name_and_commands_list[1]
+
+    if workspace_name == m.currentWorkspace {
+      return strings.Split(commands, ",")
+    }
+  }
+
+  return []string{}
+}
+
 func RenderWorkspaceListView(m model) string {
-  s := "Choose your workspace:\n"
+  s := "Choose your workspace:\n\n"
 
   for i, choice := range m.workspaces {
     cursor := " "
@@ -68,17 +96,18 @@ func RenderWorkspaceListView(m model) string {
     s += fmt.Sprintf("%s %s\n", cursor, choice)
   }
 
-  s += "\nPress q to quit.\n"
+  // s += "\nPress n to add a new workspace."
+  // s += "\nPress x to delete the selected workspace."
+  s += "\nPress q to quit."
 
   return s
 }
 
 func RenderWorkspaceDetailView(m model) string {
-  // TODO: add the name of the workspace in the title of the screen
-  s := "Choose your command:\n"
+  s := "[Workspace]: " + m.currentWorkspace + "\n"
+  s += "\nChoose your command:\n"
 
-  // TODO: fetch commands for current workspace from the kv db
-  commands := []string{"npm run dev", "echo \"hello world\""}
+  commands := m.commandsInCurrentWorkspace
 
   for i, choice := range commands {
     cursor := " "
@@ -88,7 +117,9 @@ func RenderWorkspaceDetailView(m model) string {
     s += fmt.Sprintf("%s %s\n", cursor, choice)
   }
 
-  s += "\nPress q to see all workspaces.\n"
+  // s += "\nPress n to add a new command."
+  // s += "\nPress x to delete the selected command."
+  s += "\nPress q to see all workspaces."
 
   return s
 }
@@ -121,9 +152,14 @@ func main() {
   }
 
   // Set some test workspaces as an example
-  // if err := db.Set([]byte("workspaces"), []byte("test,bro,ski")); err != nil {
-  //   log.Fatal(err)
-  // }
+  if err := db.Set([]byte("workspaces"), []byte("test,bro,ski")); err != nil {
+    log.Fatal(err)
+  }
+
+  // Set some test commands as an example
+  if err := db.Set([]byte("workspace-commands"), []byte("test:npm run dev,npm run test|bro:nx serve app|ski:yarn dev,npm run e2e")); err != nil {
+    log.Fatal(err)
+  }
 
   // Get the persisted workspaces
   v, err := db.Get([]byte("workspaces"))
@@ -132,7 +168,7 @@ func main() {
   if err != nil {
     db.Set([]byte("workspaces"), []byte(""))
     v = []byte("")
-  }
+  } 
   
   // Create a slice with workspaces from the db string value
   workspaces := strings.Split(string(v), ",")
@@ -140,6 +176,8 @@ func main() {
   initialModel := model{
     screen: "workspace-list",
     workspaces: workspaces,
+    commandsInCurrentWorkspace: []string{},
+    db: db,
   }
 
   p := tea.NewProgram(initialModel)
